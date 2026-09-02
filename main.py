3b25f954df7021d15dbe86a43b886e0d6652777b
niko <sthephanienicole1306@gmail.com>
2026-09-02 16:36:34 -0500

    inicio del proyecto AgroCBA

---
+++ main.py
@@ -0,0 +1,8 @@
+def main():
+    print("==============================")
+    print("       SISTEMA AGROCBA        ")
+    print("==============================")
+    print("Sistema iniciado correctamente")
+
+if __name__ == "__main__":
+    main()
